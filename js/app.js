0d3664fc73e18f529a25850b2068ef6c82be40d6
Alexandre Delfino <alex.delf@laposte.net>
2022-05-11 10:49:53 +0200

    avancement 1.0

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-document.addEventListener("DOMContentLoaded", e => {});
+document.addEventListener("DOMContentLoaded", e => {
+    let el = document.querySelector("nav ul li:last-of-type");
+    function funky() {
+        var element = document.body;
+        element.classList.toggle("dark-mode");
+    }
+    el.addEventListener("click", () => {
+        funky();
+    });
+});
